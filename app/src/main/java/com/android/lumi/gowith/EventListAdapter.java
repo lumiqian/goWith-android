cccb138dc6d064fecfba700c33aeab1f739bcd53
Lumi Qian <yuqian@email.com>
2017-11-07 22:22:05 +0800

    add a participants textView, participate name editText, and button

--- app/src/main/java/com/android/lumi/gowith/EventListAdapter.java
+++ app/src/main/java/com/android/lumi/gowith/EventListAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,11 +29,18 @@ public class EventListAdapter extends ArrayAdapter<String> {
         View theView = theInFlater.inflate(R.layout.event_row_layout, parent, false);
 
         String displayedEvent = getItem(position);
-        TextView eventTextView = (TextView)theView.findViewById(R.id.event_text_view_1);
+        TextView eventTextView = (TextView)theView.findViewById(R.id.event_text_view);
         eventTextView.setText(displayedEvent);
 
         ImageView imageView = (ImageView)theView.findViewById(R.id.image_view_1);
         imageView.setImageResource(R.drawable.dot);
+
+        String participants = "Participants: ";
+
+        //Todo: need to send a GET API call to get participant list and then append to participants string
+
+        TextView participantsTextView = (TextView)theView.findViewById(R.id.participants_text_view);
+        participantsTextView.setText(participants);
 
         return theView;
     }
--- app/src/main/java/com/android/lumi/gowith/MainActivity.java
+++ app/src/main/java/com/android/lumi/gowith/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -14,10 +16,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText participantNameEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        participantNameEditText = (EditText)findViewById(R.id.participant_name_edit_text);
 
         String[] listedEvents = {"Exhibition: Neighborhood Earth", "Fitness: Century Park Morning Run", "Market: Second-hand Party"};
 
@@ -37,4 +43,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public void OnGoingButtonClick(View view) {
+        String participantName = "";
+
+        if(participantNameEditText.getText().length() != 0){
+            participantName = String.valueOf(participantNameEditText.getText());
+        }
+
+        //todo: need to send a POST API call to add this participantName to this event's participants list
+
+        String yourConfirmedResponse = participantName + ", thank you for registering here!";
+        Toast.makeText(this, yourConfirmedResponse, Toast.LENGTH_SHORT).show();
+    }
+
 }
